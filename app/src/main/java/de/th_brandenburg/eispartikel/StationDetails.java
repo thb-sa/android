ea0b9bbbd095a9ea4e94f11ecafd064f02827e35
Pascal Helbig <pascalhelbig@gmail.com>
2016-11-29 16:24:33 +0100

    in StationDetails die gemessene Werte angezeigt

--- app/src/main/java/de/th_brandenburg/eispartikel/StationDetails.java
+++ app/src/main/java/de/th_brandenburg/eispartikel/StationDetails.java
@@ -2,20 +2,42 @@ package de.th_brandenburg.eispartikel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import datenKlassen.Station;
+import datenKlassen.Tageswerte;
 
 public class StationDetails extends AppCompatActivity {
+    private Station station;
+
+    @BindView(R.id.lvWerte)
+    ListView lvWerte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_details);
+        ButterKnife.bind(this);
 
-        Station station = (Station) getIntent().getSerializableExtra("station");
-
+        station = (Station) getIntent().getSerializableExtra("station");
         getSupportActionBar().setTitle(station.getStationID());
-        Toast.makeText(this, station.getStationID(), Toast.LENGTH_SHORT).show();
+        showWerte();
+    }
+
+    private void showWerte() {
+        ArrayList<String> list = new ArrayList<>();
+        for (Map.Entry<String, Tageswerte> wert : station.getAktuelleWerte().entrySet()) {
+            list.add(wert.getKey() + ": " + wert.getValue().getAktuellerWert());
+        }
+        String[] werte = list.toArray(new String[0]);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, werte);
+        lvWerte.setAdapter(arrayAdapter);
     }
 }
