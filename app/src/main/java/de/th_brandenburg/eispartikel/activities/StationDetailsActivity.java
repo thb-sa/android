d236bd3e34e995d7e5588a320a116f5881a6c340
Pascal Helbig <pascalhelbig@gmail.com>
2016-12-02 00:07:52 +0100

    Wenn die Werte zu niedrig sind werden sie rot dargestellt, sonst in Grün

--- app/src/main/java/de/th_brandenburg/eispartikel/activities/StationDetailsActivity.java
+++ app/src/main/java/de/th_brandenburg/eispartikel/activities/StationDetailsActivity.java
@@ -1,14 +1,18 @@
 package de.th_brandenburg.eispartikel.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import butterknife.BindView;
@@ -45,7 +49,7 @@ public class StationDetailsActivity extends AppCompatActivity implements NeuesOb
         showVorgabe();
         showWerte();
     }
-    
+
     @OnClick(R.id.tvVorgabe)
     protected void newWert() {
         Intent intent = new Intent(this, NeueWerteActivity.class);
@@ -58,20 +62,8 @@ public class StationDetailsActivity extends AppCompatActivity implements NeuesOb
     }
 
     private void showWerte() {
-        ArrayList<String> list = new ArrayList<>();
-        for (Map.Entry<String, Tageswerte> eintrag : station.getAktuelleWerte().entrySet()) {
-            String datum = eintrag.getKey();
-            Tageswerte wert = eintrag.getValue();
-
-            String text = datum + ": " + wert.getAktuellerWert();
-            String abweichung = "(" + Util.plusMinus(wert.getAbweichung()) + " / " + Util.plusMinus(wert.getRelativeAbweichung()) + "%)";
-
-            list.add(text + " " + abweichung);
-        }
-        String[] werte = list.toArray(new String[0]);
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, werte);
-        lvWerte.setAdapter(arrayAdapter);
+        CustomArrayAdapter adapter = new CustomArrayAdapter(this, station.getAktuelleWerte());
+        lvWerte.setAdapter(adapter);
     }
 
     @Override
@@ -80,5 +72,42 @@ public class StationDetailsActivity extends AppCompatActivity implements NeuesOb
         werte.put(aenderungsmeldung.getDatum(), aenderungsmeldung.getTageswerte());
         station.setAktuelleWerte(werte);
         showWerte();
+    }
+
+    public class CustomArrayAdapter extends ArrayAdapter<ConcurrentHashMap<String, Tageswerte>> {
+        private final Context context;
+        private final ConcurrentHashMap<String, Tageswerte> values;
+        private final String[] dates;
+
+        CustomArrayAdapter(Context context, ConcurrentHashMap<String, Tageswerte> werte) {
+            super(context, -1);
+            this.context = context;
+            this.values = werte;
+            this.dates = values.keySet().toArray(new String[0]);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.werte_row, parent, false);
+            TextView textView = (TextView) rowView.findViewById(R.id.tv);
+
+            String datum = dates[position];
+            Tageswerte wert = values.get(datum);
+
+            String text = datum + ": " + wert.getAktuellerWert();
+            String abweichung = "(" + Util.plusMinus(wert.getAbweichung()) + " / " + Util.plusMinus(wert.getRelativeAbweichung()) + "%)";
+
+            textView.setText(text + " " + abweichung);
+            textView.setTextColor(wert.getAbweichung() >= 0 ? Color.GREEN : Color.RED);
+
+            return rowView;
+        }
+
+        @Override
+        public int getCount() {
+            return dates.length;
+        }
     }
 }
